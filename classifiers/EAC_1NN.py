0fa064b621ac158abef7c1ed3969bd48f62a0b88
Gianluca Manca <gianluca.manca89@outlook.com>
2025-11-13 19:38:17 +0100

    Add: Attenuation coefficient based alarm flood classifier

---
+++ classifiers/EAC_1NN.py
@@ -0,0 +1,80 @@
+import math
+import numpy as np
+import classifiers.utils as utils
+
+class EAC_1NN:
+    """
+    Exponentially Attenuation Coefficient 1-Nearest Neighbor classifier
+    """
+
+    def __init__(self, params={"attenuation_coefficient_per_min": 0.0667}):
+        # convert attenuation coefficient from min to hour
+        self.attenuation_coefficient = (
+            params["attenuation_coefficient_per_min"] * 60
+        )
+        self.X_train_features = None
+        self.y_train = None
+
+    @property
+    def __name__(self):
+        return "EAC_1NN"
+
+    def fit(self, X, y):
+        self.n_alm_vars = X.shape[1]
+        self.y_train = y
+        # Cache converted alarms
+        X_converted = utils.convert_alarms(X)
+        self.X_train_features = self.get_feature_vectors(X_converted)
+
+    def predict_proba(self, X):
+        # Cache converted alarms
+        X_converted = utils.convert_alarms(X)
+        X_test_features = self.get_feature_vectors(X_converted)
+        return np.array([self.get_nearest_neighbor(x) for x in X_test_features])
+
+    def calculate_normalized_distances(self, reference_vector, vector_set):
+        distances = np.linalg.norm(vector_set - reference_vector, axis=1)
+        # Normalize by the maximum distance across the set
+        max_distance = np.max(distances)
+        if max_distance == 0:
+            return np.zeros_like(distances)
+        return distances / max_distance
+
+    def get_nearest_neighbor(self, x):
+        # Vectorized version to compute min distance per class
+        min_distances = []
+        for class_label in np.unique(self.y_train):
+            class_samples = self.X_train_features[self.y_train == class_label]
+            normalized_distances = self.calculate_normalized_distances(x, class_samples)
+            min_distances.append(np.min(normalized_distances))
+        return min_distances
+
+    def get_feature_vectors(self, X):
+        X_alarms = self.get_alarm_flood_vector(X)
+        X_times = self.get_time_vector(X)
+
+        X_alarms = np.array(X_alarms)
+        X_times = np.array(X_times)
+        
+        # Vectorized computation of exponential attenuation
+        X_vectors = X_alarms * np.exp(-self.attenuation_coefficient * X_times)
+        return X_vectors
+
+    def get_alarm_flood_vector(self, X):
+        # Vectorized method to create alarm flood vectors
+        X_alarms = np.zeros((len(X), self.n_alm_vars))
+        for i, x in enumerate(X):
+            alarm_types = [alm.type for alm in x]
+            X_alarms[i, alarm_types] = 1
+        return X_alarms
+
+    def get_time_vector(self, X):
+        # Vectorized method to create time vectors
+        X_times = np.full((len(X), self.n_alm_vars), np.inf)
+        for i, alarms in enumerate(X):
+            for alarm in alarms:
+                if X_times[i, alarm.type] == np.inf:  # If not already set
+                    X_times[i, alarm.type] = alarm.start
+        # Replace any remaining inf with 0 (no time set)
+        X_times[X_times == np.inf] = 0
+        return X_times
